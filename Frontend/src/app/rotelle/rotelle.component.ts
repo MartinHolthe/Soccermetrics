d3252424e42c905b8d0c42a3429f22cb50ea423e
Martin Holthe <martinholthe@hotmail.no>
2021-11-21 20:38:06 +0100

    working on chart.js labels

--- Frontend/src/app/rotelle/rotelle.component.ts
+++ Frontend/src/app/rotelle/rotelle.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Chart,registerables } from 'chart.js';
+import {Chart, registerables } from 'chart.js';
+import ChartDataLabels from 'chartjs-plugin-datalabels';
 
 @Component({
   selector: 'app-rotelle',
@@ -21,32 +22,87 @@ export class RotelleComponent implements OnInit {
     new Chart('rotelle', {
       type: 'polarArea',
       data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: ['Player Name'],
         datasets: [
           {
             label: '# of Votes',
-            data: [100, 19, 3, 5, 2, 3],
+            data: [100, 100, 100, 100, 100, 90, 90, 90, 90, 90, 80, 80, 80, 80, 80, 70, 70, 70, 70,  70, 60, 60, 60, 60, 60 ],
             backgroundColor: [
+              'rgba(255, 99, 132, 0.2)', 
               'rgba(255, 99, 132, 0.2)',
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(255, 99, 132, 0.2)',
+            
               'rgba(54, 162, 235, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+
               'rgba(255, 206, 86, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+
               'rgba(75, 192, 192, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+
               'rgba(153, 102, 255, 0.2)',
-              'rgba(255, 159, 64, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
             ],
             borderColor: [
               'rgba(255, 99, 132, 1)',
+              'rgba(255, 99, 132, 1)',
+              'rgba(255, 99, 132, 1)',
+              'rgba(255, 99, 132, 1)',
+              'rgba(255, 99, 132, 1)',             
+
               'rgba(54, 162, 235, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(54, 162, 235, 1)',
+
               'rgba(255, 206, 86, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(255, 206, 86, 1)',
+
               'rgba(75, 192, 192, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(75, 192, 192, 1)',
+
               'rgba(153, 102, 255, 1)',
-              'rgba(255, 159, 64, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(153, 102, 255, 1)',
             ],
             borderWidth: 1,
           },
         ],
       },
+      plugins: [ChartDataLabels],
       options: {
+        plugins: {
+          // Change options for ALL labels of THIS CHART
+          datalabels: {
+            color: '#36A2EB',
+            anchor: 'end'
+          }
+        },
         responsive:true,
         scales: {
           y: {
