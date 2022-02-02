ecfa903dd20856086ca11be03be1fb571b0d2c8c
MartinHolthe <46777951+MartinHolthe@users.noreply.github.com>
2022-02-02 14:29:15 +0100

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -23,7 +23,7 @@ app.get("/bundesliga", async(req, res) => {
 })
 
 //GET a row from table - try "/bundesliga/:id/:position/:minutes" + "SELECT Percentiles[dribble past], Percentiles[blocks] etc.. FROM bundesliga_percentiles WHERE id = ? AND position = ? and minutes = ?", [id, position, minutes]
-// Try a join on same table to benchmark a defender against a attacker e.g...
+// Try a join on same table to benchmark a defender against a attacker e.g... https://www.postgresqltutorial.com/postgresql-full-outer-join/
 app.get("/bundesliga/:id", async(req, res) => {
     const {id} = req.params
     try {
