1aa0a4a3cec17dd17a4c5f709a73ea4579a0b4e9
MartinHolthe <46777951+MartinHolthe@users.noreply.github.com>
2022-02-04 09:15:33 +0100

    Update bundesliga.component.ts

--- Frontend/src/app/bundesliga/bundesliga.component.ts
+++ Frontend/src/app/bundesliga/bundesliga.component.ts
@@ -25,6 +25,43 @@ export class BundesligaComponent implements OnInit {
     });
     this.chart.update();
   }
+  
+  updateChart(){
+  chart.data.dataset[0].data = [
+              //Passing stats
+              this.percentiles[selectedPlayerId]['Key_Passes_Per_90'], //How to parse this to string
+              this.percentiles[selectedPlayerId].Successful_Deliveries_Into_Box_Per_90,
+              this.percentiles[selectedPlayerId].Progressive_Passes_Per_90,
+              this.percentiles[selectedPlayerId].Pressured_Passes_Per_90,
+              this.percentiles[selectedPlayerId]['Pass_Completion_%'],
+
+              //Defending stats 
+              this.percentiles[selectedPlayerId].Dribbled_Past, //How to parse this to string
+              this.percentiles[selectedPlayerId]['Rate_Adj_Tackles_Won_%'],
+              this.percentiles[selectedPlayerId]['Pressure_Regain_%'],
+              this.percentiles[selectedPlayerId].Interceptions_Per_90,
+              this.percentiles[selectedPlayerId].Ball_recoveries_Per_90,
+              this.percentiles[selectedPlayerId]['Rate_Adj_Aerials_Won_%'],
+
+              //Attacking stats 
+              this.percentiles[selectedPlayerId].Netto_xA, //How to parse this to string
+              this.percentiles[selectedPlayerId].xG_Per_Shot,
+              this.percentiles[selectedPlayerId].Non_Penalty_xA_xG_Per_90,
+              this.percentiles[selectedPlayerId].Non_Penalty_xG_Per_90,
+              this.percentiles[selectedPlayerId].xA_Per_90,
+              this.percentiles[selectedPlayerId].SCA_Per_90,
+              this.percentiles[selectedPlayerId].GCA_Per_90,
+
+              //Possesion stats 
+              this.percentiles[selectedPlayerId].Touches_in_Box, //How to parse this to string
+              this.percentiles[selectedPlayerId].Final_Third_Entries,
+              this.percentiles[selectedPlayerId].Progressive_Distance_Per_Carry,
+              this.percentiles[selectedPlayerId].Rate_Adj_Successful_Dribbles_Per_90,
+              this.percentiles[selectedPlayerId].Turnovers_Per_90,
+              this.percentiles[selectedPlayerId].Rate_Adj_Target_of_an_Attempted_Pass
+            ];
+    chart.update();
+  }
 
   onPlayerSelected(selectedPlayerId: number): void {
     console.log('Selected player: ', this.percentiles[selectedPlayerId]);
